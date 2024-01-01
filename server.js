571d804f972ba32ce870914b06482c0e9811ba14
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-01-01 16:51:08 +0000

    changed cors settings to *:*

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "https://saucedontsauce.github.io/XOjs/"
+        origin: "*:*"
     }
 });
 
