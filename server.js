2f7b0d825ef1b0627a18508d3820c58fdf5ad959
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-01-01 17:36:03 +0000

    changes for render

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ const userList = {}
 
 io.on('connection', (socket) => {
     console.log('someone connected');
-    console.log('person who connected :', socket)
+    console.log('person who connected :', socket.id)
 
 
     io.emit("list_users", "test")
 })
 
+io.on('connect_error', (err)=>{
+    console.log(err);
+});
+io.on('connect_failed', (err)=>{
+    console.log(err);
+});
+io.on('disconnect', (err)=>{
+    console.log(err);
+});
 
-io.listen(3000)
+
+io.listen(8080)
