292a4aaed8441aa44392d77e9919370492595fb6
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-01-01 16:28:58 +0000

    message

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 const io = new Server();
 
 const userList = {}
 
-io.on('connection', (socket)=>{
+io.origins('*:*')
+
+io.on('connection', (socket) => {
     console.log('someone connected');
     console.log('person who connected :', socket)
-    
+
 
     io.emit("list_users", socket)
 })
