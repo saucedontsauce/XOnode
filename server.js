7b0488491a6335a0ec31766ddf7b49df0486d08e
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-01-02 23:32:16 +0000

    added some initial front end

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const path = require('path');
 
 let app = express();
 const port = 3000;
@@ -9,7 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 
 let liveUsers = {};
 
-app.get("/", (req, res) => {
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+
+app.get("/api", (req, res) => {
   res.status(200).send({
     success: true,
     message: "welcome to the beginning of greatness",
@@ -32,18 +39,27 @@ io.on("connection", (socket) => {
     "userName": 'user',
     level: 0
   };
+  let idArr = Object.keys(liveUsers);
+let userCount = idArr.length;
+console.log(userCount)
+  io.emit('x0_Live_Users', userCount);
+
 
   socket.on('disconnect', ()=>{
     console.log('user disconnected', socket.id);
     delete liveUsers[socket.id]
   })
 
-  socket.on('x1_Live_Users', ()=>{
-    console.log('player count requested from',socket.id);
+  socket.on('req_count', ()=>{
     let idArr = Object.keys(liveUsers);
-    let userCount = idArr.length;
-    io.emit('x0_Live_Users, ', userCount);
+let userCount = idArr.length;
+    console.log('player count requested from',socket.id);
+    io.emit('x0_Live_Users', userCount);
 })
+
+    socket.on("req_searching", ()=>{
+        console.log('searching list requested')
+    })
 
 });
 
