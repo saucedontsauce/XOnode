43bb65e78564b53dddca5b05a5b1bb579c83128c
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-01-05 23:00:02 +0000

    game page works and game system started on

--- public/scripts/socketEvents.js
+++ public/scripts/socketEvents.js
@@ -1,29 +1,125 @@
- // socket callers and listeners
- const socket = io()
+// variables
+const socket = io()
 
- let userCount = 0;
+let userCount = 0;
 let gameCount = 0;
+let thisGame;
 
- socket.on('x0_Live_Users', (msg) => {
-     console.log(msg);
-     userCount = msg ; 
-     let displaylist = document.getElementsByClassName('userCount');
-     for(let i=0; i<displaylist.length; i++){
-         displaylist[i].textContent = userCount;
-     };
- });
+// screenManagement
+const showScreen = (targId, reqfunc) => {
+    let allScreens = document.getElementsByClassName('gameView');
+    let targeted = document.getElementById(targId);
+    for (let i = 0; i < allScreens.length; i++) {
+        let thisOne = allScreens[i];
+        let test = thisOne.classList.contains('hidden');
+        if (!test) {
+            thisOne.classList.toggle('hidden');
+        } else {
 
- socket.on('X0_Live_Games', (msg)=>{
-    console.log(msg);
-    gameCount = msg ; 
-    let displaylist = document.getElementsByClassName('gameCount');
-    for(let i=0; i<displaylist.length; i++){
-        displaylist[i].textContent = gameCount
+        };
+    };
+    targeted.classList.toggle('hidden');
+    if(reqfunc){
+        reqfunc()
     }
- })
+};
 
- const startSearch = () => {
+
+// player maker
+
+
+
+class gameUser {
+    constructor() {
+        this.id = socket.id;
+        this.username = "imAuser";
+        this.inGame = false;
+        this.game = null;
+        this.runChecks()
+    }
+    runChecks() {
+        let storedUsername = localStorage.getItem('username');
+        if(storedUsername){
+            console.log('username previously used:', storedUsername);
+            this.username = storedUsername;
+        }
+    }
+
+}
+
+
+let thisUsr = new gameUser
+
+
+// socket callers and listeners
+socket.on('x0_Live_Users', (msg) => {
+    userCount = msg;
+    let displaylist = document.getElementsByClassName('userCount');
+    for (let i = 0; i < displaylist.length; i++) {
+        displaylist[i].textContent = userCount;
+    };
+});
+
+socket.on('x0_Live_Games', (msg) => {
+    console.log("live games", msg)
+    let displaylist = document.getElementsByClassName('gameCount');
+    console.log(displaylist)
+    for (let i = 0; i < displaylist.length; i++) {
+        console.log(displaylist[i])
+        displaylist[i].textContent = msg;
+    };
+})
+socket.on("x0_Game_Redirect", (msg)=>{
+    console.log(msg)
+    showScreen(`${msg}`)
+    resizegrid()
+})
+
+const startSearch = () => {
     let thisUsrName = localStorage.getItem('username');
     socket.emit('req_searching', thisUsr);
     console.log("%s searching", thisUsrName);
- }
+}
+
+
+
+
+const resizegrid = () => {
+    let grid = document.getElementById('gameGrid');
+    let childarr = grid.children;
+    grid.height = grid.offsetWidth
+    let celldim = grid.offsetWidth / 3
+    for (let i = 0; i < childarr.length; i++) {
+        let rowid = i + "";
+        let thisRow = document.getElementById(rowid)
+        thisRow.style.height = celldim + "px";
+        for (let j = 0; j < thisRow.children.length; j++) {
+            let thiscellid = i + "" + j;
+            let thiscell = document.getElementById(thiscellid);
+            thisRow.children[j].style.width = celldim + "px";
+            thisRow.children[j].style.height = celldim + "px";
+        }
+    }
+}
+
+const playerWins = (winner) => {
+    document.getElementById('winnerDisp').textContent = winner;
+    document.getElementById('winnerBanner').style.display = 'flex';
+}
+
+const moveMade = ({coord, symb}) => {
+    let targ = document.getElementById(coord);
+    targ.innerHTML = symb;
+}
+
+
+window.onload = resizegrid;
+window.onresize = resizegrid;
+const handlePlayClick = (e) => {
+    moveMade(e.target.id, "X")
+
+}
+
+
+
+
--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const path = require('path');
 
+
+class Game {
+  constructor(p1, p2) {
+    this.p1id = p1.id;
+    this.p2id = p2.id;
+    this.p1name = p1.username;
+    this.p2name = p2.username;
+    this.gameField = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ]
+  };
+
+  makeMove() {
+
+  }
+}
+
+
 let app = express();
 const port = 3000;
 app.use(express.json());
@@ -10,8 +30,9 @@ app.use(express.urlencoded({ extended: true }));
 
 let liveUsers = {};
 let liveGames = {};
+let searchingUsers = {};
 
-app.use('/public', express.static(path.join(__dirname,'/public')))
+app.use('/public', express.static(path.join(__dirname, '/public')))
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
@@ -37,10 +58,7 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   console.log("We are live and connected");
   console.log(socket.id);
-  liveUsers[socket.id] = {
-    "userName": 'user',
-    level: 0
-  };
+  liveUsers[socket.id] = { ...socket };
   let idArr = Object.keys(liveUsers);
   let userCount = idArr.length;
   console.log(userCount);
@@ -53,27 +71,91 @@ io.on("connection", (socket) => {
     let idArr = Object.keys(liveUsers);
     let userCount = idArr.length;
     io.emit('x0_Live_Users', userCount);
+    handleCleanup(socket)
   })
 
   socket.on('req_count', () => {
-    let idArr = Object.keys(liveUsers);
-    let userCount = idArr.length;
+    let usrArr = Object.keys(liveUsers);
+    let userCount = usrArr.length;
     console.log('player count requested from', socket.id);
     io.emit('x0_Live_Users', userCount);
   })
 
-  socket.on('req_games', ()=>{
-
+  socket.on('req_games', () => {
+    let gamesArr = Object.keys(liveGames);
+    let gameCount = gamesArr.length;
+    console.log('game count requested from', socket.id)
+    io.emit('x0_Live_Games', gameCount)
   })
 
   socket.on("req_searching", (msg) => {
     console.log(msg.username, "wants to search ...");
-    console.log(msg)
-  })
-
+    console.log(msg);
+    liveUsers[socket.id].username = msg.username;
+    searchingUsers[socket.id] = liveUsers[socket.id];
+    findMatch(socket.id)
+  });
 });
 
 
+// weird functions / classes
+
+
+
+
+const handleCleanup = (user) => {
+  console.log(user.id, 'needs cleanup')
+}
+
+const findMatch = (id) => {
+  console.log('find a match')
+  for (let i in searchingUsers) {
+    console.log(searchingUsers[i].id)
+    if (searchingUsers[i].id != id) {
+      console.log("match found")
+      // match found
+      delete searchingUsers[i];
+      delete searchingUsers[id];
+      io.to(i).emit('x0_Game_Redirect', "gamePlay");
+  io.to(id).emit('x0_Game_Redirect', "gamePlay");
+      manageGame(id, i)
+    }
+  }
+
+}
+
+const manageGame = (p1, p2) => {
+  console.log('game started');
+  let gameName = p1 + p2;
+  let thisgame = new Game(p1, p2);
+  liveGames[gameName] = {...thisgame};
+  let gameArr = Object.keys(liveGames);
+  let gameCount = gameArr.length;
+  io.emit("x0_Live_Games", gameCount);
+  io.to(p1.id).emit('x0_Game_Live', thisgame);
+  io.to(p2.id).emit('x0_Game_Live', thisgame);
+
+  io.on("game_move", (move) => {
+
+  })
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// run server
 
 
 httpServer.listen(port, () => {
